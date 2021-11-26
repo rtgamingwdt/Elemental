91478ac0d651622873909daf396408c75d4ce634
RT <rustytrim11@gmail.com>
2021-11-26 23:51:35 +0000

    Don't know what changed with this file github?

--- src/main/java/me/rtgamingwdt/elemental/item/ItemInit.java
+++ src/main/java/me/rtgamingwdt/elemental/item/ItemInit.java
@@ -1,10 +1,11 @@
 package me.rtgamingwdt.elemental.item;
 
 import me.rtgamingwdt.elemental.Elemental;
+import net.minecraft.world.item.CreativeModeTab;
 // import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
-// import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -12,8 +13,17 @@ public class ItemInit {
     
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Elemental.MOD_ID);
 
-    // public static final RegistryObject<Item> RED_BERYL = ITEMS.register("red_beryl", 
-    // () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    //TODO Obtain Demonite from killing gods or demons.
+    public static final RegistryObject<Item> DEMONITE = ITEMS.register("demonite", 
+    () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> EARTHSTONE = ITEMS.register("earthstone", 
+    () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> FIRESTONE = ITEMS.register("firestone", 
+    () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> ICESTONE = ITEMS.register("icestone", 
+    () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> LIGHTNINGSTONE = ITEMS.register("lightningstone", 
+    () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
