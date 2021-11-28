23917d8270543afb383bc90e0edfc92b4e13810e
Mezo <dominik@koch-bautechnik.de>
2021-11-28 01:39:21 +0100

    Changed Icon of the mod tab

--- src/main/java/me/rtgamingwdt/elemental/item/ModCreativeModeTab.java
+++ src/main/java/me/rtgamingwdt/elemental/item/ModCreativeModeTab.java
@@ -1,5 +1,6 @@
 package me.rtgamingwdt.elemental.item;
 
+import me.rtgamingwdt.elemental.block.BlockInit;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -8,7 +9,7 @@ public class ModCreativeModeTab {
     public static final CreativeModeTab ELEMENTAL_TAB = new CreativeModeTab("elemental_tab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ItemInit.DEMONITE.get());
+            return new ItemStack(BlockInit.LIGHTNINGSTONE_ORE.get());
         }
     };
 }
